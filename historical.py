eb0762b05d7197d118a426a406d342b64448b6ff
crogar <58400421+crogar@users.noreply.github.com>
2021-06-29 16:23:50 -0700

    historical data

--- historical.py
+++ historical.py
@@ -2,9 +2,10 @@ import json
 import requests
 import pandas as pd
 from pprint import pprint
+from config import keys
 
 def get_historical(ticker="AAPL"):
-    url = f"https://financialmodelingprep.com/api/v3/historical-price-full/{ticker}?apikey=14f90359650cd4c61f0f33af0a7564d6"
+    url = f"https://financialmodelingprep.com/api/v3/historical-price-full/{ticker}?apikey={keys[0]}"
     result = requests.get(url).json()
     df = pd.DataFrame(result['historical'])
     print(result['symbol'])
@@ -12,6 +13,6 @@ def get_historical(ticker="AAPL"):
     print(df.shape)
     print(df.iloc[0])
     df = df[["date","open","high","low","close"]]
-    df.to_csv("historical_csv.csv",index=False)
+    df.to_csv("./Historical_Data/historical_FB.csv",index=False)
 
-get_historical("AAPL")
+get_historical("FB")
