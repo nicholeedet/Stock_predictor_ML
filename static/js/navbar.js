a31db052c36d1b4b2e7f810554c405a5b67ae878
crogar <58400421+crogar@users.noreply.github.com>
2021-06-30 15:51:09 -0700

    animated navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,3 +1,15 @@
 const mainMenu = document.querySelector('.mainMenu')
 const closeMenu = document.querySelector('.closeMenu')
 const openMenu = document.querySelector('.openMenu')
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close_);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top= '-15px';
+}
+
+function close_(){
+    mainMenu.style.top = '-105%';
+}
