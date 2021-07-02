1814f8f4235c3232dd0a2356b31e7be4c2fbcecf
crogar <58400421+crogar@users.noreply.github.com>
2021-07-01 21:09:06 -0700

    adding suggestions.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,12 +1,19 @@
 // populating autocom-box
-$.get( "http://127.0.0.1:5000/list_tickers", function( data ) {
-    // console.log(data)
-    var ul = document.getElementsByClassName("tickers-list");
-    data.forEach(function (tick, index) {
-        // console.log(tick);
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(tick));
-        ul.appendChild(li);
-      });
-    
-  });
+// let suggestions=[];
+
+// $.get( "http://127.0.0.1:5000/list_tickers", function( data ) {
+//     // console.log(data)
+//      suggestions = data;
+//   });
+
+//   console.log(suggestions)
+url = "http://127.0.0.1:5000/list_tickers"
+
+async function getData(url) {
+    const response = await fetch(url);
+  
+    return response.json();
+  }
+  
+  let data = getData(url);
+  console.log(data)
