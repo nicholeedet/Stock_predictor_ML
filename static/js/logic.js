fa60195a51a81b5108ed6d94bb5108adcb4ff707
crogar <58400421+crogar@users.noreply.github.com>
2021-07-01 18:21:37 -0700

    autocomplete bar

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,9 @@
 // populating autocom-box
 $.get( "http://127.0.0.1:5000/list_tickers", function( data ) {
-    console.log(data)
+    // console.log(data)
+    data.forEach(function (tick, index) {
+        console.log(tick);
+        $('#tickers-list').append('<li>hello</li>');
+      });
+    
   });
