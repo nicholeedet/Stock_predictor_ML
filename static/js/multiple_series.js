323514e1e61d386f215d8c943d57139986e86e2a
crogar <58400421+crogar@users.noreply.github.com>
2021-07-07 20:07:18 -0700

    Update multiple_series.js

---
+++ static/js/multiple_series.js
@@ -0,0 +1,33 @@
+var chart = LightweightCharts.createChart(document.body, {
+	width: 600,
+  height: 300,
+	rightPriceScale: {
+		visible: true,
+    borderColor: 'rgba(197, 203, 206, 1)',
+	},
+	leftPriceScale: {
+		visible: true,
+    borderColor: 'rgba(197, 203, 206, 1)',
+	},
+	layout: {
+		backgroundColor: '#ffffff',
+		textColor: 'rgba(33, 56, 77, 1)',
+	},
+  grid: {
+    horzLines: {
+      color: '#F0F3FA',
+    },
+    vertLines: {
+      color: '#F0F3FA',
+    },
+  },
+	crosshair: {
+		mode: LightweightCharts.CrosshairMode.Normal,
+	},
+	timeScale: {
+		borderColor: 'rgba(197, 203, 206, 1)',
+	},
+	handleScroll: {
+		vertTouchDrag: true,
+	},
+});
