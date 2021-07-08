415b4045bca9f7e6568bd6196beaa473ecbd2833
crogar <58400421+crogar@users.noreply.github.com>
2021-07-07 20:14:47 -0700

    multiple series

--- static/js/multiple_series.js
+++ static/js/multiple_series.js
@@ -32,10 +32,11 @@ var chart = LightweightCharts.createChart(document.body, {
 	},
 });
 
+ticker = document.title;
 // Fetching api data from our flask app
-fetch('http://127.0.0.1:5000/get_historical/AAPL')
+fetch('http://127.0.0.1:5000/get_historical/' + ticker)
 	.then((r) => r.json())
 	.then((response) => {
 		console.log(response);
-		areaSeries.setData(response);
+
 	});
